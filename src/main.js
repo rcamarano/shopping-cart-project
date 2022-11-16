d74f8d4285525feff0503158fbb49f111cb430f4
Rogerio Camarano <rogeriocamarano@gmail.com>
2022-11-16 17:23:19 -0300

    fix some linter issues

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductList } from './helpers/cepFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,7 +1,6 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
 import computadorSearch from './mocks/search';
-
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('verifica se fetchProductsList é uma função', () => {
