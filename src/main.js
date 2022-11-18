e3b7f1a6e7dd932f59e8974fec6d341cf5a773a8
Rogerio Camarano <rogeriocamarano@gmail.com>
2022-11-18 18:07:07 -0300

    req 4 and 5 done

--- src/main.js
+++ src/main.js
@@ -5,13 +5,39 @@ import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
+const container = document.querySelector('.products');
+const createParagraph = document.createElement('p');
+const errorAlert = document.createElement('h1');
+
+const createErrorAlert = () => {
+  errorAlert.className = 'error';
+  errorAlert.textContent = 'Algum erro ocorreu, recarregue a página e tente novamente';
+  container.appendChild(errorAlert);
+};
+
+const createLoading = () => {
+  container.appendChild(createParagraph);
+  createParagraph.className = 'loading';
+  createParagraph.textContent = 'carregando...';
+};
+
+const stopLoading = () => {
+  createParagraph.remove();
+};
+
 const createProductList = async () => {
-  const listItems = await fetchProductsList('computador');
-  const listProducts = document.querySelector('.products');
-  listItems.forEach((element) => {
-    const listElements = createProductElement(element);
-    listProducts.appendChild(listElements);
-  });
+  createLoading();
+  try {
+    const listItems = await fetchProductsList('computador');
+    const listProducts = document.querySelector('.products');
+    listItems.forEach((element) => {
+      const listElements = createProductElement(element);
+      listProducts.appendChild(listElements);
+    });
+  } catch (error) {
+    stopLoading();
+    createErrorAlert();
+  }
 };
 
 window.onload = () => {
