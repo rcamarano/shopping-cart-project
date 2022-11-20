40aaf917e03c214e620d6086c2010f87f97aa360
Rogerio Camarano <rogeriocamarano@gmail.com>
2022-11-20 00:40:01 -0300

    req 8and 9, another attempt

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
-import { fetchProductsList } from './helpers/fetchFunctions';
+import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
 import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
 import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 
@@ -9,6 +9,7 @@ document.querySelector('.cep-button').addEventListener('click', searchCep);
 const container = document.querySelector('.products');
 const createParagraph = document.createElement('p');
 const errorAlert = document.createElement('h1');
+const getList = document.querySelector('.cart__products');
 
 const createErrorAlert = () => {
   errorAlert.className = 'error';
@@ -42,10 +43,34 @@ const createProductList = async () => {
   }
 };
 
+const getTotalPrice = document.querySelector('.total-price');
+
+const sumTotalPrices = () => {
+  let sum = 0;
+  getSavedCartIDs().map((elementId) => Promise.all([fetchProduct(elementId)])
+    .then((response) => {
+      sum += response[0].price;
+      getTotalPrice.textContent = sum.toFixed(2);
+      localStorage.setItem('cartPrice', sum.toFixed(2));
+    }));
+};
+
+getList.addEventListener('click', (event) => {
+  console.log(event.target.innerText);
+  const sub = event.target.parentNode.lastElementChild.lastElementChild.innerHTML;
+  const number1 = Number(sub);
+  const number2 = Number(getTotalPrice.innerHTML);
+  const result = number2 - number1;
+  getTotalPrice.innerHTML = '';
+  getTotalPrice.innerHTML = result;
+});
+
 const prductsButton = document.querySelector('.products');
+
 prductsButton.addEventListener('click', async (event) => {
   console.log(event.target.parentNode.firstChild.innerText);
   const productId = event.target.parentNode.firstChild.innerText;
+
   saveCartID(productId);
   const result = await fetchProduct(productId);
   const getList = document.querySelector('.cart__products');
@@ -62,5 +87,6 @@ const recoverdItem = () => {
 
 window.onload = () => {
   createProductList();
+  sumTotalPrices();
   recoverdItem();
 };
